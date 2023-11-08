129ea39d9f93130ae1ba697603f7b0835217ca8f
Mohit Kumar <mohitkmr170@gmail.com>
2023-11-09 01:51:40 +0530

    feat: folder structuring

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View style={styles.parentContainer}>
+        <Text>Instarem currency app</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  parentContainer: {
+    flex: 1,
+  },
+});
+
+export default App;
